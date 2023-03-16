67951030e8a91f101d87b4f6a9f85f3a1484084f
Alec-litho <112494715+Alec-litho@users.noreply.github.com>
2023-03-16 21:25:04 +0300

    y

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 let express = require('express')
 let fs = require('fs');
 
+
+fs.readFile('./data/posts.json','utf8', (err, data) => {
+    
+})
+
+
 const cors = require("cors");
 let app = express()
 app.use(cors());
 
+app.post('/', (req,res) => {
+    req.on('data', (data) => {
+        fs.writeFile('./data/posts.json', data.toString(), (err) => {
+            console.log(err);
+        })
+    })
+    res.sendFile('C:/Users/opall/OneDrive/Рабочий стол/programming/JavaScript/3 React/project lesson/src//data/posts.json')
+})
 app.get('/', (req,res) => {
     res.sendFile('C:/Users/opall/OneDrive/Рабочий стол/programming/JavaScript/3 React/project lesson/src//data/posts.json')
 })
