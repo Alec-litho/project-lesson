9f1a777e0b61087af74e7b3a2db597db3a572c4d
Alec-litho <112494715+Alec-litho@users.noreply.github.com>
2023-03-19 16:44:15 +0300

    y

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,53 +1,64 @@
 import classes from '../style_modules/gallery.module.css'
-import photos from '../data/myPictures/photoData.json'
 import { ReactComponent as Arrow } from '../icons/arrow.svg'
 import {useState, useEffect, useRef} from 'react'
 import axios from 'axios'
 import albums from '../data/myPictures/albums.json'
-let myPhotos = axios.get('https://api.imgbb.com/1/upload?expiration=600&key=432e8ddaeeb70d2d1be863e87c0f354e')
-.then(res => console.log(res))
-let key = '432e8ddaeeb70d2d1be863e87c0f354e'
-
+let callfunc = false
 export default function Gallery() {
+    
     let leftArrow = useRef(null),
     rightArrow = useRef(null),
     addPicture = useRef(null),
     underlines = useRef([<input value={'text'}/>])
-    let [currentPicture, setPicture] = useState([...photos])
+    let [currentPictures, setPictures] = useState(false)
+    let [isLoaded, finishLoading] = useState(false)
+    if(callfunc) savePicture(currentPictures)
+    useEffect(()=> {
+        fetchData()
+        callfunc = true
+    },[isLoaded])
 
-    let test = addPicture.current.value.slice(12)
-    savePicture(currentPicture)
-
+    async function fetchData() {
+        let data = await axios.get('http://localhost:3001/pictures').then(res => res.data)
+        setPictures(data)
+        finishLoading(true)
+    }
     function doAnimation(e) {e.target.childNodes.forEach(child => {
         underlines.current.forEach(item => item.id === child.id?  item.style.width = 200 + 'px' : null)
     })}
     function removeAnimation(e) {underlines.current.forEach(underline => underline.style.width = 100 + 'px')}
 
     function savePicture(picture) {
-            let date = new Date()
-            let day = date.getDate()
-            let month = date.getMonth() + 1
-            axios.post('http://localhost:3001/pictures', JSON.stringify(picture)).then(res => console.log(res)).catch(err => console.log(err))
+        axios.post('http://localhost:3001/pictures', JSON.stringify(picture))
     }
     function showArrows(e) {e.target.id === "left"? leftArrow.current.style.display = 'block' : rightArrow.current.style.display = 'block'}
     function hideArrows(e) {e.target.id === "left"? leftArrow.current.style.display = 'none' : rightArrow.current.style.display = 'none'}
 
 
     async function uploadPicture(e) {
-        console.log(addPicture.current.files[0]);
-        let test = addPicture.current.value.slice(12)
+        let imgName = addPicture.current.value.slice(12)
+
         const rf = new FileReader();
         rf.readAsDataURL(addPicture.current.files[0])
         rf.onloadend = function (event) {
-        const body = new FormData();
-        body.append("image", event.target.result.split(",").pop()); 
-        body.append("name", test.substring(0, test.lastIndexOf('.')));
-        fetch('https://api.imgbb.com/1/upload?expiration=600&key=432e8ddaeeb70d2d1be863e87c0f354e', {
-            method: "POST",
-            body: body
-        }).then(res => {
-           setPicture(prev => [...prev, {name: test.substring(0, test.lastIndexOf('.')), uploaded: day + '.' + month, year: date.getFullYear()}])
-        })}
+            const body = new FormData();
+            body.append("image", event.target.result.split(",").pop()); 
+            body.append("name", imgName.slice(0, imgName.lastIndexOf('.')));
+            fetch('https://api.imgbb.com/1/upload?expiration=600&key=432e8ddaeeb70d2d1be863e87c0f354e', {
+                method: "POST",
+                body: body
+            }).then(res => res.json()).then(res => {
+                let date = new Date()
+                let day = date.getDate()
+                let month = date.getMonth() + 1
+                setPictures(prev => [...prev, {name: imgName.slice(0, imgName.lastIndexOf('.')), displayURL: res.data.display_url  ,uploaded: day + '.' + month, year: date.getFullYear()}])
+            })
+        }
+    }
+    if(isLoaded === false) {
+        console.log('wait');
+        return <Wait/>
+        
     }
     return (
         <div className={classes.gallery}>
@@ -72,10 +83,16 @@ export default function Gallery() {
                <Arrow ref={rightArrow} className={classes.arrowRight}/>
                <div id='left' className={classes.leftBorder} onMouseEnter={showArrows} onMouseLeave={hideArrows}></div>
                <div id='right' className={classes.rightBorder} onMouseEnter={showArrows} onMouseLeave={hideArrows}></div>
-               {/* {photos.map((photo, id) => {
-                return <img src={require(`../data/myPictures/${photo.name}.jpg`)} key={id}></img>
-               })} */}
+                    {
+                    currentPictures.map((photo, id) => {
+                        return <img src={photo.displayURL} key={id}></img>
+                    })
+                    }
             </div>
         </div>
     )
+}
+
+function Wait() {
+    <div>Nothing is loaded yet</div>
 }
