367a523b4fab4094bed6e2127ca95c5e8cef0f3b
Alec-litho <112494715+Alec-litho@users.noreply.github.com>
2023-05-30 23:16:56 +0300

    y

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,8 +4,8 @@ let {registerValidation, loginValidation, postCreateValidation, imageValidation}
 const cors = require("cors");
 const {checkAuth} = require('./utils/checkAuth')
 const {register, login, getMe} = require('./controllers/UserController')
-const {create, getAll, getOne, deletePost, update, getMyPosts, getPostImages} = require('./controllers/postController')
-const {uploadImage, getOneImage, getAllImages, getOneAlbum, getAlbums, uploadAlbum, deleteImage, getMyAlbums} = require('./controllers/imageController')
+const {create, getAll, getOne, deletePost, update, getMyPosts, getPostImages, getComments, postComment, postReply, deleteComment} = require('./controllers/postController')
+const {uploadImage, getOneImage, getAllImages, getOneAlbum, getAlbums, uploadAlbum, deleteImage, getMyAlbums, updateImage} = require('./controllers/imageController')
 const multer = require('multer')
 
 mongoose
@@ -34,14 +34,19 @@ app.post('/auth/register', registerValidation, register)
 //post paths----------------------------------------
 app.get('/posts', getAll)
 app.get('/posts/:id', getOne)
+app.post('/posts', checkAuth, postCreateValidation, create) 
 app.post('/posts/images', getPostImages)
 app.post('/posts/myposts', getMyPosts) 
-app.post('/posts', checkAuth, postCreateValidation, create) 
 app.delete('/posts/:id', checkAuth, deletePost)
 app.patch('/posts',checkAuth, update)//dont forget to finish it
+// app.get('/posts/comments/:id', getComments)
+app.post('/posts/comments', postComment)
+app.post('/posts/comments/:id', postReply)
+app.delete('/posts/comments/:id', deleteComment)
 //images paths---------------------------------------
 app.get('/images',getAllImages)
 app.get('/images/:id', getOneImage)
+app.post('/images/update/:id', updateImage)
 app.delete('/images/:id', checkAuth, deleteImage)
 app.post('/images', checkAuth, imageValidation, uploadImage)
 app.post('/albums/myalbums', checkAuth, imageValidation, getMyAlbums)
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,27 @@
 import { ReactComponent as Delete } from '../assets/icons/delete.svg'
 import { ReactComponent as Change } from '../assets/icons/change.svg'
+import { ReactComponent as Share } from '../assets/icons/share.svg'
+import { ReactComponent as Like } from '../assets/icons/like.svg'
+import { ReactComponent as Views } from '../assets/icons/views.svg'
+import { ReactComponent as Comments } from '../assets/icons/comments.svg'
+
 import classes from '../styles/post.module.css'
 import Slider from './Slider'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import MessageTool from './MessageTool'
+import axios from 'axios'
 export default function Post(props) {
   let [sliderTrue, setSliderTrue] = useState(false)
   let [currPictureId, setCurrPictureId] = useState(false)
+  let [commentsTrue, setCommentsTrue] = useState(false)
+  let [comment, setComment] = useState(null)
+  useEffect(_ => {
+    if(comment!==null) {
+      axios.post('http://localhost:3001/posts/comments')
+    }
+  },[comment])
+
+
     return (
         <div className={classes.post}>
         <div className={classes.postHeader}>
@@ -19,7 +35,6 @@ export default function Post(props) {
         <div className={classes.text}>{props.text}</div>
           <div className={classes.images}>
             {props.images.map(img => {
-              console.log(img);
               return <div className={classes.imgWrapper}><img data-id={img._id} className={classes.image} onClick={e => {
                 setCurrPictureId(e.target.dataset.id)
                 setSliderTrue(true)
@@ -28,9 +43,67 @@ export default function Post(props) {
           }
           </div>
           <div className={classes.tools}>
-
+          <div className={classes.tool}>
+               <p>{props.views}</p>
+               <Views className={classes.view}/>
+              </div>
+            <div className={classes.rightBlock}>
+              <div className={classes.tool}>
+                <p>{props.commentsNum}</p>
+                <Comments className={classes.icon} onClick={_ => setCommentsTrue(prev => !prev)}/>
+              </div>
+              <div className={classes.tool}>
+                <p>{props.likes}</p>
+                <Like className={classes.icon}/>
+              </div>
+              <div className={classes.tool}>
+                <p>{props.share}</p>
+                <Share className={classes.icon}/>
+              </div>
+            </div>
+          </div>
+          <div className={commentsTrue? classes.comments : classes.commentsHide}>
+            {props.comments.length>0 & props.comments.map((comment, id) => {
+              return ( 
+              <div key={id}>
+                <Comment autherPicture={comment.autherPicture} autherName={comment.autherName} likes={comment.likes} comment={comment.comment} time={comment.time}/>
+                <div>
+                  <a href="#">Show replies</a>
+                  <div className={classes.replies}>
+                    {comment.replies.map((reply,id) => {
+                      return <Comment key={id} autherPicture={reply.autherPicture} autherName={reply.autherName} likes={reply.likes} comment={reply.comment} time={reply.time}/>
+                    })}
+                  </div>
+                </div>
+              </div>)
+            })}
+            <MessageTool uploadMessage={setComment}/>
           </div>
           <Slider sliderTrue={sliderTrue} setSliderTrue={setSliderTrue} currPictureId={currPictureId}/>
         </div>
     )
 }
+
+function Comment(props) {
+  return (
+    <div className={classes.comment}>
+    <div className={classes.commentMain}>
+    <img className={classes.profilePicture} src={props.autherPicture}></img>
+     <div className={classes.commentBody}>
+       <h3>{props.autherName}</h3>
+       <p>{props.comment}</p>
+       <div className={classes.commentInf}>
+         <p className={classes.time}>{props.time}</p>
+         <a href='#'>Reply</a>
+       </div>
+     </div>
+     <div className={classes.likes}>
+      <Like/>
+      <p>{props.likes}</p>
+     </div>
+    </div>
+
+  </div>
+  )
+}
+
